a292cad3b72b6c09ecce843c504edbadf8a430a0
Prabash <mvsprabash@gmail.com>
2026-09-12 15:39:45 +0530

    feat(UI): add a blank glfw window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main() {
     std::cout << "Legion Control Panel\n";
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to init glfw\n";
+        return 1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(
+        800,
+        600,
+        "Legion Control Panel",
+        nullptr,
+        nullptr
+    );
+
+    if (!window) {
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
